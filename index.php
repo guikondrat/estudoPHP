840e1ddfb96c53bca6849aff7e505921d10fd29e
guikondrat <53410547+guikondrat@users.noreply.github.com>
2022-11-27 21:29:07 -0300

    Add estudo de arrays, variaveis, print, tipagem

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+$Jarbas = array(
+    1 => 'A',
+    2 => 'B',
+    3 => 'C',
+    4 => 'D',
+);
+$Empregados = array('Juca', 'Gaucho', 'Almir');
+//print $Jarbas;  
+//print "O homem morre pelo pecado {$Jarbas[2]}"
+print "O empregado do ano foi o {$Empregados[1]}";
+?>
